3b54b2328786b666267f4f3d1935ccd2b2a24caa
netooo <kazushi.mitsue@gmail.com>
2021-02-24 22:58:11 +0900

    Add: room manage

--- backend/room.go
+++ backend/room.go
@@ -1,8 +1,10 @@
 package backend
 
 import (
+	"context"
 	"github.com/google/uuid"
 	"github.com/netooo/go-tutorial-gopherjs/app/models"
+	"log"
 	"sync"
 	"time"
 )
@@ -16,4 +18,34 @@ type Room struct {
 	timer      *time.Timer
 	cancel     func()
 	once       sync.Once
+}
+
+var (
+	newRoomCh = make(chan *models.Room)
+	delRoomCh = make(chan uuid.UUID, 1)
+	getRoomCh = make(chan getRoom)
+)
+
+type getRoom struct {
+	uuid   string
+	result chan *Room
+}
+
+func roomManage(ctx context.Context) {
+	rooms := map[string]*Room{}
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case u := <-delRoomCh:
+			rooms[u.String()].Close()
+			delete(rooms, u.String())
+			log.Println("del room:", u.String())
+		case room := <-newRoomCh:
+			rooms[room.UUID.String()] = NewRoom(context.Background(), room)
+			log.Println("new room:", room.UUID.String())
+		case req := <-getRoomCh:
+			req.result <- rooms[req.uuid]
+		}
+	}
 }
